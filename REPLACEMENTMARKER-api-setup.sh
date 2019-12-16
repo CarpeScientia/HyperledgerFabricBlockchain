21f14ec6274812d184ffc97fc699cd22a9d416b6
CarpeScientia <CarpeScientia@users.noreply.github.com>
2019-12-16 14:00:51 +0100

    Update REPLACEMENTMARKER-api-setup.sh
    ca startup fix

--- REPLACEMENTMARKER-api-setup.sh
+++ REPLACEMENTMARKER-api-setup.sh
@@ -1,9 +1,25 @@
 
+
+echo "parameters: $1 $2 $3 $4"
+if [ -z "$2" ]
+then
+        echo "parameters are: CHANNEL_NAME  CONTRACT_NAME "
+        echo ""
+        exit 1
+fi
+export CHANNEL_NAME=$1
+export CCODE_NAME=$2
+
+cd first-network/
+
+export CA0KEY=$(ls -rt1 ./crypto-config/peerOrganizations/org1.REPLACEMENTMARKER.com/ca/|grep _sk|tail -n1)
+export CA1KEY=$(ls -rt1 ./crypto-config/peerOrganizations/org2.REPLACEMENTMARKER.com/ca/|grep _sk|tail -n1)
+export CAPASSWD=somepassword
 # in fabric-samples/first-network/
 
 ./ccp-generate-REPLACEMENTMARKER.sh 
 
-sudo docker-compose -f docker-compose-ca-REPLACEMENTMARKER.yaml up -d
+sudo -E docker-compose -f docker-compose-ca-REPLACEMENTMARKER.yaml up -d
 
 
 #sudo docker logs -f --tail=200 ca_peerOrg1
